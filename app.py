44d5a24b188774e748d1197f12d368a32caa8e65
Yousha Saibi <youshasaibi187@gmail.com>
2025-04-26 12:58:51 +0500

    added vehicle detection model

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ st.markdown(
 )
 
 main_page=st.Page("main.py",title="Home",icon="🎈")
-page_1=st.Page("page1.py",title="Page 1",icon = "❄️")
-page_2=st.Page("page2.py",title="Page 2",icon = "❄️")
-page_3=st.Page("page3.py",title="Page 3",icon = "❄️")
-page_4=st.Page("page4.py",title="Page 4",icon = "❄️")
+page_1=st.Page("features/page1.py",title="Page 1",icon = "❄️")
+page_2=st.Page("features/page2.py",title="Page 2",icon = "❄️")
+page_3=st.Page("features/page3.py",title="Page 3",icon = "❄️")
+page_4=st.Page("features/page4.py",title="Page 4",icon = "❄️")
 pg=st.navigation([main_page,page_1,page_2,page_3,page_4])
 pg.run()
